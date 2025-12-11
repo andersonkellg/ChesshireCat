db5b3d28f29633a1497c6ff0c281059dc0a50e39
andersonkellg <anderson.kell.g@gmail.com>
2025-12-10 23:06:02 -0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+// sw.js
+const CACHE_NAME = 'chesshire-cat-v1';
+const ASSETS = [
+  './',
+  './ChesshireCat.html',
+  './manifest.webmanifest',
+  './ChesshireCat.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
